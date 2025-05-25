b140fee115c0474e8324bbfc6b978edf742b134a
Maxim Mig <maxim.mig@jetbrains.com>
2025-05-25 17:03:44 +0200

    Clean up

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,16 @@
 import {defineConfig} from 'vite';
-import {resolve} from 'path';
 
 export default defineConfig({
-  publicDir: 'public',
   build: {
     target: 'es2022',
     rollupOptions: {
       input: {
-        background: resolve(__dirname, 'src/background.js'),
-        content: resolve(__dirname, 'src/content.js')
+        background: './src/background.js',
+        content: './src/content.js'
       },
       output: {
         entryFileNames: '[name].js',
       }
-    },
-    outDir: 'dist',
-    emptyOutDir: true,
+    }
   }
 });
