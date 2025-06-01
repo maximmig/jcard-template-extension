17ada923e459e45078522d9b7b732e2ac75f91a3
Maxim Mig <maxim.mig@jetbrains.com>
2025-06-01 20:04:54 +0200

    Import Bandcamp release

--- src/models/BandcampRelease.js
+++ src/models/BandcampRelease.js
@@ -4,4 +4,26 @@ export default class BandcampRelease extends Release {
   constructor(document) {
     super(document);
   }
+
+  initFromDocument() {
+    const ldJson = this._document.head.querySelector('script[type="application/ld+json"]');
+    const data = JSON.parse(ldJson.textContent);
+
+    const {image, name} = data.albumRelease.find(release => release['@type'].includes('Product'));
+    this._coverUrl = image;
+    this._title = name;
+
+    this._artist = data.byArtist.name;
+
+    const releaseDate = new Date(data.datePublished);
+    if (!isNaN(releaseDate.valueOf())) {
+      this._released = releaseDate.toLocaleString('en-US', { month: 'long', year: 'numeric' });
+    }
+
+    const {itemListElement} = data.track;
+    const tracksPerSide = Math.ceil(itemListElement.length / 2);
+    const tracks = itemListElement.map(listItem => listItem.item.name);
+    this._sideA = tracks.slice(0, tracksPerSide).join('\n');
+    this._sideB = tracks.slice(tracksPerSide).join('\n');
+  }
 }
