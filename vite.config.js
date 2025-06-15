6a7c7b68c703e2c9eaac51ec470cf19c3100f55f
Maxim Mig <maxim.mig@jetbrains.com>
2025-06-15 12:57:18 +0200

    [chore] Generate source maps

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,33 @@
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 
-export default defineConfig({
-  build: {
-    target: 'es2022',
-    rollupOptions: {
-      input: {
-        background: './src/background.js',
-        content: './src/content.js',
+export default defineConfig(({ mode }) => {
+  const isDevMode = mode === 'dev';
+
+  return {
+    build: {
+      target: 'es2022',
+      rollupOptions: {
+        input: {
+          background: './src/background.js',
+          content: './src/content.js',
+        },
+        output: {
+          entryFileNames: '[name].js',
+        },
       },
-      output: {
-        entryFileNames: '[name].js',
-      },
+      minify: isDevMode ? false : 'esbuild',
+      sourcemap: isDevMode,
     },
-    minify: 'esbuild',
-  },
-  plugins: [
-    eslint({
-      cache: false,
-      fix: true,
-    }),
-  ],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-  },
+    plugins: [
+      eslint({
+        cache: false,
+        fix: true,
+      }),
+    ],
+    test: {
+      environment: 'jsdom',
+      globals: true,
+    },
+  };
 });
