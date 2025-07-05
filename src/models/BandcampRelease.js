b180ad4376d7af73876cddb9310a1a3cd7501901
Maxim Mig <maxim.mig@jetbrains.com>
2025-07-05 16:59:23 +0200

    Clean up releases

--- src/models/BandcampRelease.js
+++ src/models/BandcampRelease.js
@@ -2,31 +2,62 @@ import Release from './Release.js';
 
 export default class BandcampRelease extends Release {
   initFromDocument() {
+    const bandcampData = this._getBandcampData();
+    const release = this._getRelease(bandcampData);
+
+    this._coverUrl = this._getCoverUrl(release);
+    this._title = this._getTitle(release);
+    this._artist = this._getArtist(bandcampData);
+    this._released = this._getReleased(bandcampData);
+
+    ({ sideA: this._sideA, sideB: this._sideB } =
+      this._getTrackSides(bandcampData));
+  }
+
+  _getBandcampData() {
     const ldJson = this._document.head.querySelector(
       'script[type="application/ld+json"]',
     );
-    const data = JSON.parse(ldJson.textContent);
+    return JSON.parse(ldJson.textContent);
+  }
 
-    const { image, name } = data.albumRelease.find((release) =>
+  _getRelease(bandcampData) {
+    return bandcampData.albumRelease.find((release) =>
       release['@type'].includes('Product'),
     );
-    this._coverUrl = Array.isArray(image) ? image[0] : image;
-    this._title = name;
+  }
 
-    this._artist = data.byArtist.name;
+  _getCoverUrl(release) {
+    const { image } = release;
+    return Array.isArray(image) ? image[0] : image;
+  }
 
-    const releaseDate = new Date(data.datePublished);
-    if (!Number.isNaN(releaseDate.valueOf())) {
-      this._released = releaseDate.toLocaleString('en-US', {
-        month: 'long',
-        year: 'numeric',
-      });
-    }
+  _getTitle(release) {
+    return release.name;
+  }
 
-    const { itemListElement } = data.track;
+  _getArtist(bandcampData) {
+    return bandcampData.byArtist.name;
+  }
+
+  _getReleased(bandcampData) {
+    const releaseDate = new Date(bandcampData.datePublished);
+    return Number.isNaN(releaseDate.valueOf())
+      ? undefined
+      : releaseDate.toLocaleString('en-US', {
+          month: 'long',
+          year: 'numeric',
+        });
+  }
+
+  _getTrackSides(bandcampData) {
+    const { itemListElement } = bandcampData.track;
     const tracksPerSide = Math.ceil(itemListElement.length / 2);
     const tracks = itemListElement.map((listItem) => listItem.item.name);
-    this._sideA = tracks.slice(0, tracksPerSide).join('\n');
-    this._sideB = tracks.slice(tracksPerSide).join('\n');
+
+    return {
+      sideA: tracks.slice(0, tracksPerSide).join('\n'),
+      sideB: tracks.slice(tracksPerSide).join('\n'),
+    };
   }
 }
--- src/models/DiscogsRelease.js
+++ src/models/DiscogsRelease.js
@@ -2,73 +2,104 @@ import Release from './Release.js';
 
 export default class DiscogsRelease extends Release {
   initFromDocument() {
-    const discogsData = JSON.parse(
-      this._document.getElementById('dsdata').textContent,
-    );
-    const entries = Object.entries(discogsData.data.ROOT_QUERY);
+    const discogsData = this._getDiscogsData();
+    const release = this._getRelease(discogsData);
+
+    this._coverUrl = this._getCoverUrl(discogsData, release);
+    this._title = this._getTitle(release);
+    this._artist = this._getArtist(release);
+    this._released = this._getReleased(release);
+
+    ({ sideA: this._sideA, sideB: this._sideB } = this._getTrackSides(
+      discogsData,
+      release,
+    ));
+  }
+
+  _getDiscogsData() {
+    return JSON.parse(this._document.getElementById('dsdata').textContent).data;
+  }
+
+  _getRelease(discogsData) {
+    const entries = Object.entries(discogsData.ROOT_QUERY);
     const [, releaseEntryKeyHolder] =
       entries.find(
         ([key]) => key.startsWith('release') || key.startsWith('masterRelease'),
       ) ?? [];
     const releaseEntryKey = releaseEntryKeyHolder.__ref;
 
-    const release = discogsData.data[releaseEntryKey];
-    const finalRelease =
-      'keyRelease' in release // check if it's a master release
-        ? discogsData.data[release.keyRelease.__ref]
-        : release;
-    const { title, primaryArtists, released, tracks } = finalRelease;
+    const release = discogsData[releaseEntryKey];
+    return 'keyRelease' in release // check if it's a master release
+      ? discogsData[release.keyRelease.__ref]
+      : release;
+  }
 
-    const imagesEntryKey = Object.keys(finalRelease).find((k) =>
+  _getCoverUrl(discogsData, release) {
+    const imagesEntryKey = Object.keys(release).find((k) =>
       k.startsWith('images'),
     );
-    const images = imagesEntryKey ? finalRelease[imagesEntryKey] : null;
+    const images = imagesEntryKey ? release[imagesEntryKey] : null;
     if (images) {
       const imageNodeKey =
         images.edges.length > 0 ? images.edges[0].node.__ref : null;
       if (imageNodeKey) {
-        const coverImageKey = discogsData.data[imageNodeKey]?.fullsize?.__ref;
+        const coverImageKey = discogsData[imageNodeKey]?.fullsize?.__ref;
         if (coverImageKey) {
-          this._coverUrl = discogsData.data[coverImageKey].sourceUrl;
+          return discogsData[coverImageKey].sourceUrl;
         }
       }
     }
 
-    this._title = title;
+    return undefined;
+  }
 
-    if (primaryArtists.length > 0) {
-      this._artist = primaryArtists
-        .reduce((acc, { displayName, joiningText }) => {
-          acc.push(displayName);
-          if (joiningText) {
-            acc.push(joiningText);
-          }
-          return acc;
-        }, [])
-        .join(' ');
-    }
+  _getTitle(release) {
+    return release.title;
+  }
 
-    const [year, month] = released.split('-');
+  _getArtist(release) {
+    return release.primaryArtists
+      .reduce((acc, { displayName, joiningText }) => {
+        acc.push(displayName);
+        if (joiningText) {
+          acc.push(joiningText);
+        }
+        return acc;
+      }, [])
+      .join(' ');
+  }
+
+  _getReleased(release) {
+    const [year, month] = release.released.split('-');
     const releaseDate = new Date(Number(year), Number(month) - 1, 1);
-    if (!Number.isNaN(releaseDate.valueOf())) {
-      this._released = releaseDate.toLocaleString('en-US', {
-        month: 'long',
-        year: 'numeric',
-      });
-    }
 
-    const trackNames = tracks
+    return Number.isNaN(releaseDate.valueOf())
+      ? undefined
+      : releaseDate.toLocaleString('en-US', {
+          month: 'long',
+          year: 'numeric',
+        });
+  }
+
+  _getTrackSides(discogsData, release) {
+    const trackNames = release.tracks
       .map(({ __ref }) => {
-        const track = discogsData.data[__ref];
+        const track = discogsData[__ref];
         return track.trackType === 'TRACK' && track.position !== 'Video'
           ? track.title
           : null;
       })
       .filter((t) => t !== null);
-    if (trackNames.length > 0) {
-      const tracksPerSide = Math.ceil(trackNames.length / 2);
-      this._sideA = trackNames.slice(0, tracksPerSide).join('\n');
-      this._sideB = trackNames.slice(tracksPerSide).join('\n');
+
+    if (trackNames.length === 0) {
+      return { sideA: '', sideB: '' };
     }
+
+    const tracksPerSide = Math.ceil(trackNames.length / 2);
+
+    return {
+      sideA: trackNames.slice(0, tracksPerSide).join('\n'),
+      sideB: trackNames.slice(tracksPerSide).join('\n'),
+    };
   }
 }
